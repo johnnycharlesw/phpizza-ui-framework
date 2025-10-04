836ed9ad6290061c615ba7ff9b4f96d02383712a
John Charles Woods <woods.johnny.charles@gmail.com>
2025-10-04 14:26:55 -0400

    Added JS to this framework

---
+++ phpizza-ui.js
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview PHPizza UI Framework JavaScript
+ * @author John Charles Woods
+ * @version 1.0.0
+ * @license AGPL-3.0-or-later
+ */
+
+
+import './fa';
+import './labels';
+import './buttons';
+import './fieldsets';
+//qodo-gen no-import-css-in-js
